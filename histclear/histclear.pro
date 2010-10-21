500199a1ea2fb2679ecd3d5fb9ed369b3512463e
(no author) <(no author)@76bea514-bf2f-4ad3-ac3e-82dc02406d2c>
2010-10-21 15:19:05 +0000

    В бібліотеки додано проект histclear - програма очищення історії. Це треба інсталювати як мінімум на центрифуги
    git-svn-id: file:///tmp/qt4/lib@359 76bea514-bf2f-4ad3-ac3e-82dc02406d2c

---
+++ histclear/histclear.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2010-10-21T17:47:33
+#
+#-------------------------------------------------
+
+QT       += sql
+
+QT       -= gui
+
+TARGET = histclear
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ histclear/main.cpp
@@ -0,0 +1,42 @@
+#include <QtCore/QCoreApplication>
+#include <QSettings>
+#include <QDebug>
+#include <QStringList>
+#include <QtSql>
+#include <QDateTime>
+
+
+int main(int argc, char *argv[])
+{
+    QSettings set("histclear.conf",QSettings::IniFormat);
+
+    QSqlDatabase dbs=QSqlDatabase::addDatabase("QMYSQL","logging");
+
+
+    dbs.setHostName(set.value("/db/host","localhost").toString());
+    dbs.setDatabaseName(set.value("/db/db","test").toString());
+    dbs.setUserName(set.value("/db/username","scada").toString());
+    dbs.setPassword(set.value("/db/passwd","").toString());
+
+    if(dbs.open()) // спробувати відкрити
+    {
+        int tm=QDateTime::currentDateTime().toTime_t()-90*24*3600;
+        QSqlQuery qry(dbs);
+
+        set.beginGroup("table");
+        QStringList k=set.allKeys();
+        foreach(QString s,k)
+        {
+            if(!qry.exec(QString("DELETE FROM %1 WHERE Dt<%2").arg(set.value(s).toString()).arg(tm)))
+            {
+                qDebug() << qry.lastError().databaseText();
+            }
+        }
+        set.endGroup();
+        dbs.close();
+    }
+    else
+    {
+        qDebug() << "Can\'t open database" << dbs.lastError().driverText();
+    }
+}
