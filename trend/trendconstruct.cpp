801d0ea7f9f1bea38603995d0941964d04554108
vanessa <vanessa@76bea514-bf2f-4ad3-ac3e-82dc02406d2c>
2011-01-21 12:51:40 +0000

    Реалізовано сортування тегів по групах
    git-svn-id: file:///tmp/qt4/lib@374 76bea514-bf2f-4ad3-ac3e-82dc02406d2c

--- trend/trendconstruct.cpp
+++ trend/trendconstruct.cpp
@@ -2,6 +2,7 @@
 #include "ui_trendconstruct.h"
 #include <QWidget>
 #include <QTreeWidgetItem>
+#include <QHash>
 
 #include "../ionetclient/IoNetClient.h"
 
@@ -25,17 +26,32 @@ TrendConstruct::TrendConstruct(IoNetClient &source,QWidget *parent) :
     for(int i=0;i<src.size();++i)
     {
         //qDebug() << src[i]->getTags().keys();
-
+        QHash<QString,QTreeWidgetItem*> groups;
         QVector<QTreeWidgetItem*> t;
         tags.append(t);
         QTreeWidgetItem *_qtwi = new QTreeWidgetItem(ui->treeTag);
         _qtwi->setText(0,QString(tr("ПЛС %1")).arg(i));
         ui->treeTag->addTopLevelItem(_qtwi);
+
         foreach(QString tag,src[i]->getTags().keys())
         {
-            if(src[i]->isLogging(tag))
+            if(src[i]->isLogging(tag)) // якщо тег записується в історію тоді завантажити його в список
             {
-                QTreeWidgetItem *_twc = new QTreeWidgetItem(_qtwi);
+                QTreeWidgetItem *p; // батьківська група
+                QString g=tag.split("_")[0]; // визначити префікс
+                if(groups.contains(g))
+                {
+                    p=groups[g];
+                }
+                else
+                {
+                    p= new QTreeWidgetItem(_qtwi);
+                    _qtwi->addChild(p);
+                    p->setText(0,g);
+                    groups[g]=p;
+                }
+
+                QTreeWidgetItem *_twc = new QTreeWidgetItem(p);
                 _twc->setText(0,tag);
                 _twc->setText(1,src[i]->getDescription(tag));
                 tags[i] << _twc;
